cddf27a473bbff27ed95dfb9c3e238eea41af532
sahin <sahinokdem@gmail.com>
2025-07-30 10:58:05 +0300

    Added mock api req

--- obj/Debug/net8.0/GoBest.AssemblyInfo.cs
+++ obj/Debug/net8.0/GoBest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoBest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc81d1406688ccd45873a560dd14d51421629569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b74481702b26d94aa9a9ea5645801f77427bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoBest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoBest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
